7636b52ba716b17c5b98dccf46b381188a41a26d
Ruslan Pashayev <pashayevrn@gmail.com>
2023-04-23 13:07:25 -0500

    update

--- server.py
+++ server.py
@@ -13,6 +13,9 @@ def process_form():
     session['dojo_last_name'] = request.form['last_name']
     session['dojo_city'] = request.form['city']
     session['dojo_language'] = request.form['language']
+    session['dojo_black_belt'] = request.form['black_belt']
+    session['dojo_github_account'] = request.form['github_account']
+    session['dojo_fav_color'] = request.form['fav_color']
     session['dojo_comment'] = request.form['comment']
     print(request.form)
     return redirect('/result')
